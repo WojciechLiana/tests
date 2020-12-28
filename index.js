0d0890224e5d3f584e13c238b7bd934ca5b12045
wojtek <wojtek.liana@gmail.com>
2020-12-28 17:38:00 +0100

    5

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 console.log('git');
 console.log('second commit');
 console.log('third');
-<<<<<<< HEAD
 console.log('fourth');
 console.log('amend');
-=======
-console.log('fourth');
->>>>>>> 8a82389d165da15a8c06e19e4c1bb648c661956c
+console.log('fifth');
